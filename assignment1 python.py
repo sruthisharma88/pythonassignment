c4ef2f51b7b9c3d1ef1c1eee29dada91fd49fbdf
sruthisharma88 <142141117+sruthisharma88@users.noreply.github.com>
2023-08-18 17:36:53 +0530

    Add files via upload

---
+++ assignment1 python.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1
+
+# In[1]:
+
+
+'''Integer-6
+string-'hello'
+operators-* / + -
+float- -87.8'''
+
+
+# ## 2 strings & variables 
+# 
+
+# In[ ]:
+
+
+'''
+string- strings are set of characters that are enclosed in single, double or triple quotations
+variable- variable is a named location used to store data in memory.In python, you declare and change the value of the
+variable when at any time'''
+
+
+# ### 3 data types
+
+# In[ ]:
+
+
+'''Data types are used to define what type of data is stored in variables 
+3 main data types in python are 
+1.lists- an ordered collection of items.mutable, enclosed in []
+2.Tuple-immutable ,enclosed in ()
+3.Dictionaries-unordered collection of elements. contains key-value pair mutable , enclosed in {}
+
+
+
+INTEGER(int)- whole numbers without factorial component
+BOOLEAN(bool)- True,False
+strings(str)-squence of characters
+floats(float)- decimal numbers'''
+
+
+# # 4 expressions
+
+# In[ ]:
+
+
+'''expressions are made up of the mathematical operators. They evaluate values as the result. 
+example: A=10
+B=20
+A+B=C is an expression 
+print(c)
+output:30 '''
+
+
+# # 5 expression vs statement
+
+# In[ ]:
+
+
+'''statement is a logical instruction that the python interperter can read and execute.
+while expressions are made up of operators.they evaluate results'''
+
+
+# # 6
+
+# In[2]:
+
+
+bacon=22
+bacon+1
+
+
+# # 7
+
+# In[8]:
+
+
+'spam'+'spamspam'
+
+
+# In[11]:
+
+
+'spam'*3
+
+
+# # 8
+
+# In[ ]:
+
+
+'''numbers cannot be used as variables, so 100 is invalid.'''
+
+
+# # 9
+
+# In[ ]:
+
+
+'''for integer- int()
+float- float()
+string- printf()'''
+
+
+# # 10
+
+# In[13]:
+
+
+print('i have eaten 99 burritos')
+
+
+'''    'i have eaten +99+ burritos'   
+error +  ''' 
+
